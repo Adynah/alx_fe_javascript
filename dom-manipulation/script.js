1fff1f1712d7d9eb07966543dc85c5ccdea279be
adynah <t.barimalaa@gmail.com>
2025-07-03 19:15:08 +0100

    Built a Dynamic Content Generator with Advanced DOM Manipulation

---
+++ dom-manipulation/script.js
@@ -0,0 +1,50 @@
+let quotes = [{text: "The journey of a thousand miles begins with one step.", category: "Motivation"}];
+
+function showRandomQuotes() {
+    const quoteDisplay = document.getElementById("quoteDisplay");
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+
+    quoteDisplay.textContent = `"${quote.text}" - [${quote.category}]`;
+}
+
+function createAddQuoteForm() {
+    const formContainer = document.createElement("div");
+    
+    const quoteInput = document.createElement("input")
+    quoteInput.type = "text";
+    quoteInput.id = "newQuoteText";
+    quoteInput.placeholder = "Enter a new quote";
+
+    const categoryInput = document.createElement("input");
+    categoryInput.type = "text";
+    categoryInput.id = "newQuoteCategory";
+    categoryInput.placeholder = "Enter quote category";
+
+    const addButton = document.createElement("button");
+    addButton.textContent = "Add Quote";
+    addButton.addEventListener("click", () => {
+        const text = quoteInput.value.trim();
+        const category = categoryInput.value.trim();
+
+        quotes.push({ text, category});
+
+        quoteInput.value = "";
+        categoryInput.value = "";
+    });
+
+    formContainer.appendChild(quoteInput);
+    formContainer.appendChild(categoryInput);
+    formContainer.appendChild(addButton);
+
+    document.body.appendChild(formContainer);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const showQuoteButton = document.getElementById("newQuote");
+    showQuoteButton.addEventListener("click", showRandomQuote);
+
+    // Initialize
+    showRandomQuote();
+    createAddQuoteForm();
+});
